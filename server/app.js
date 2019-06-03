fe2fe51b18510fcd9fab1c02d91568fafc2d0b70
Tony Fung <tfung5@gmail.com>
2019-06-03 15:37:21 -0400

    Set up scaffold for routes/api

--- server/app.js
+++ server/app.js
@@ -6,8 +6,8 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var campusesRouter = require("./routes/campuses");
-var studentsRouter = require("./routes/students");
+var campusesRouter = require("./routes/api/campuses");
+var studentsRouter = require("./routes/api/students");
 
 var app = express();
 
@@ -23,8 +23,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/campuses", campusesRouter);
-app.use("/students", studentsRouter);
+app.use("/api/campuses", campusesRouter);
+app.use("/api/students", studentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
