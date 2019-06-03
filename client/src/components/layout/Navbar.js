42d363c50320d2999e47034d87f1524ad8c7ff83
Tony Fung <tfung5@gmail.com>
2019-06-03 15:51:29 -0400

    Fixed navbar component,took it out of homepage

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,22 +1,27 @@
+import React, { Component } from "react";
 
-import React, { Component } from 'react'
-
-export function navBar() {
-    return <nav>
-         <div className="logo-and-links">
-        <div className="logo-box">
-          <img src="" alt="Logo" className="logo" />
-        </div>
-        <div className="row">
-          <ul className="main-nav">
-            <li>
-              <a href="#us">All Campuses</a>
-            </li>
-            <li>
-              <a href="#mission">All Students</a>
-            </li>
+class Navbar extends Component {
+  render() {
+    return (
+      <nav>
+        <div className="logo-and-links">
+          <div className="logo-box">
+            <img src="" alt="Logo" className="logo" />
+          </div>
+          <div className="row">
+            <ul className="main-nav">
+              <li>
+                <a href="#us">All Campuses</a>
+              </li>
+              <li>
+                <a href="#mission">All Students</a>
+              </li>
             </ul>
-            </div>
-            </div>
-        </nav>;
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
+
+export default Navbar;
