ff09e8890db2bb91465778ebf24d59f43bbb418a
Tony Fung <tfung5@gmail.com>
2019-06-04 16:02:54 -0400

    Working on allStudents

--- client/src/components/students/AllStudents.js
+++ client/src/components/students/AllStudents.js
@@ -1,29 +1,58 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class AllStudents extends Component {
   constructor() {
     super();
     this.state = {
       students: [
-        { id: 0, name: "John Doe", campus: "Hunter College" },
-        { id: 1, name: "John Foe", campus: "Brooklyn College" },
-        { id: 2, name: "John Goe", campus: "Baruch College" }
+        {
+          id: 0,
+          name: "David Cohen",
+          campus: "Hunter College",
+          imageURL: "../../img/david-cohen.jpg"
+        },
+        { id: 1, name: "Zoey Smith", campus: "Brooklyn College", imageURL: "" },
+        { id: 2, name: "John Goe", campus: "Baruch College", imageURL: "" }
       ]
     };
   }
   render() {
     return (
-      <div>
+      <div style={{ textAlign: "center" }}>
         <h1>All Students</h1>
-        {this.state.students.map(student => (
-          <div className="studentBox">
-            <img />
-            <p>
-              <a href="/id/">Student Name</a>
-            </p>
-            <p>Campus Name</p>
-          </div>
-        ))}
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center"
+          }}
+        >
+          {this.state.students.map(student => (
+            <div
+              className="studentBox"
+              style={{
+                border: "1px solid black",
+                padding: "25px",
+                margin: "25px",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center"
+              }}
+            >
+              <img
+                src={require("../../img/david-cohen.jpg")}
+                style={{ height: "250px", width: "250px" }}
+              />
+              <p>
+                <Link to="/id/">
+                  <h2>{student.name}</h2>
+                </Link>
+              </p>
+              <h3>{student.campus}</h3>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
