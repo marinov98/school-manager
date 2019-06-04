cf116496f407af04c9561d33c32bdb73ba0b7cf9
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2019-06-03 21:47:46 -0400

    #1 for loop in Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import  Homepage  from "./components/layout/Homepage.js"
-//import NavBar from "./components/layout/Navbar.js"
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Homepage from "./components/layout/Homepage.js";
+import "./App.css";
 
 function App() {
-  return (
-    <Homepage></Homepage>
-  );
+  return <Homepage />;
 }
 
 export default App;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: [
+        { id: 1, content: "All Campuses" },
+        { id: 2, content: "All Students" }
+      ]
+    };
+  }
+
   render() {
     return (
       <nav>
@@ -10,12 +21,11 @@ class Navbar extends Component {
           </div>
           <div className="row">
             <ul className="main-nav">
-              <li>
-                <a href="#us">All Campuses</a>
-              </li>
-              <li>
-                <a href="#mission">All Students</a>
-              </li>
+              {this.state.options.map(option => (
+                <li key={option.id}>
+                  <a href="something">{option.content}</a>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
