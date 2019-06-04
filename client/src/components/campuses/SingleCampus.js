19fac779dde4105e8130d942a5e7a4b817b0405b
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2019-06-04 15:45:39 -0400

    creating singlecampus file

---
+++ client/src/components/campuses/SingleCampus.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SingleCampus extends Component{
+    render(){
+        return( <h1> Single Campus </h1>);
+    }
+}
+
+export default SingleCampus;
