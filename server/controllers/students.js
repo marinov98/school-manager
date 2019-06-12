fcb206b6ed310612e452e6ebe5c0dc63984d1225
unknown <tfung5@gmail.com>
2019-06-12 02:04:47 -0400

    Linked add new student form to database

--- server/controllers/students.js
+++ server/controllers/students.js
@@ -2,25 +2,20 @@ const Student = require("../models").Student;
 
 module.exports = {
   create(req, res) {
-    return Student.create({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      imageURL: req.body.imageURL,
-      gpa: req.body.gpa,
-      campus: req.body.campus
-    })
-      .then(student => res.status(201).send(student))
-      .catch(error => res.status(400).send(error));
+    return Student.create(req.body)
+      .then(student => res.status(201).json(student))
+      .catch(error => res.status(400).json(error));
   },
   list(req, res) {
-    return Student.findAll()
-      .then(students => res.status(200).send(students))
-      .catch(error => res.status(400).send(error));
+    return Student.findAll({
+      order: [["id", "DESC"]]
+    })
+      .then(students => res.status(200).json(students))
+      .catch(error => res.status(400).json(error));
   },
   update(req, res) {
     return Student.update(req.body, { where: { id: req.body.id } })
-      .then(student => res.status(200).send(student))
-      .catch(error => res.status(400).send(error));
+      .then(student => res.status(200).json(student))
+      .catch(error => res.status(400).json(error));
   }
 };
