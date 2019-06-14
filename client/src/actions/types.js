49477c804448d4a33308300fb2cf1626bb48d5e7
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2019-06-13 22:54:01 -0400

    get campuses is now exported

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,6 @@
 export const GET_STUDENTS = "GET_STUDENTS";
 export const ADD_STUDENT = "ADD_STUDENT";
+export const GET_CAMPUSES = "GET_CAMPUSES";
 export const DELETE_STUDENT = "DELETE_STUDENT";
 export const ADD_CAMPUS = "ADD_CAMPUS";
 export const DELETE_CAMPUS = "DELETE_CAMPUS";
